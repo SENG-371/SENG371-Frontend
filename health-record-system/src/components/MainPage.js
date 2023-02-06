902f06e941a95c71f30ee307a9b970e3162fc0e1
jackson <jacksonkonkin@gmail.com>
2023-02-06 11:29:06 -0800

    cleaned up project files

---
+++ health-record-system/src/components/MainPage.js
@@ -0,0 +1,13 @@
+import '../index.css';
+
+function MainPage() {
+  return (
+    <div className="Main-Page">
+      <header className="Main-Page-header">
+        <h1>Modern Health Record System</h1>
+      </header>
+    </div>
+  );
+}
+
+export default MainPage;
