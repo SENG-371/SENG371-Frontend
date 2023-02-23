7b848350f5f570f465769c0d6b7834831e15db19
seyedmehradadimi <seyedmehradadimi@uvic.ca>
2023-02-22 16:24:05 -0800

    Changed Navbar

--- health-record-system/src/index.js
+++ health-record-system/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import MainPage from './components/MainPage';
-import NavBar from './components/NavBar';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <NavBar />
+    <ResponsiveAppBar />
     <MainPage />
   </>
 );
