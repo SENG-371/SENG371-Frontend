85414230c6b626dee950eac106c9917613d692d1
seyedmehradadimi <seyedmehradadimi@uvic.ca>
2023-03-04 14:52:45 -0800

    Added record component and dashboard page

--- health-record-system/src/App.js
+++ health-record-system/src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import MainPage from './components/MainPage';
 import SignInPage from './components/SignInPage';
-import ResponsiveAppBar from './components/ResponsiveAppBar';
+import Dashboard from './components/Dashboard'
 
 export default function App() {
   return (
@@ -11,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="signIn" element={<SignInPage />} />
+        <Route path="dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   );
