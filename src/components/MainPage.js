aa82cd345ef6c73b15aefb42d7fed8dc310cc8f6
seyedmehradadimi <seyedmehradadimi@uvic.ca>
2023-03-21 15:09:50 -0700

    readded the main page

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,15 +1,23 @@
-import '../index.css';
-import { EHR } from '../assets'
-import ResponsiveAppBar from './ResponsiveAppBar'
-import OverviewPage from './Overview/OverviewPage';
-
+import "../index.css";
+import { EHR } from "../assets";
+import ResponsiveAppBar from "./ResponsiveAppBar";
 
 function MainPage() {
-
   return (
     <>
-      <ResponsiveAppBar />
-      <OverviewPage />
+      <ResponsiveAppBar></ResponsiveAppBar>
+      <div className="Main-Page">
+        <header className="Main-Page-header">
+          <h1 style={{ fontWeight: "bold", marginTop: 40 }}>
+            Modern Health Record System
+          </h1>
+          <h2 style={{ margin: 30, fontWeight: "lighter", fontSize: 25 }}>
+            Dear practitioners, please login to your profile to access the
+            health records
+          </h2>
+          <img src={EHR} width="40%" style={{ margin: 60 }}></img>
+        </header>
+      </div>
     </>
   );
 }
