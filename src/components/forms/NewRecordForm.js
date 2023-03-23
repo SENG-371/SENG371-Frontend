b80c9536c78837a9cfcaffc247437d0e8a1fcc19
jackson <jacksonkonkin@gmail.com>
2023-03-23 10:01:59 -0700

    forgot to switch branches for popup addition

--- src/components/forms/NewRecordForm.js
+++ src/components/forms/NewRecordForm.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
+import PopUp from '../ui/PopUp';
+import Backdrop from '../ui/Backdrop';
 import classes from './NewPatientForm.module.css'
 import Card from '../ui/Card'
 
@@ -9,6 +11,8 @@ function NewRecordForm(props) {
   const ridInputRef = useRef();
   const descriptionInputRef = useRef();
   const dateInputRef = useRef();
+
+  const [popUpIsOpen, setPopUpIsOpen] = useState(false)
 
   function submitHandler(event) {
     event.preventDefault();
@@ -28,31 +32,45 @@ function NewRecordForm(props) {
     props.onAddRecord(recordData);
   }
 
+  function handleClick() {
+    setPopUpIsOpen(true)
+  }
+
+  function closeModalHandler() {
+    setPopUpIsOpen(false);
+  }
 
   return (
-    <Card>
-      <form className={classes.form} onSubmit={submitHandler}>
-        <div className={classes.control}>
-          <label htmlFor='title'>Title</label>
-          <input type="text" required id="title" ref={titleInputRef}></input>
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='description'>Description</label>
-          <textarea id='description' required rows='5' ref={descriptionInputRef}></textarea>
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='rid'>Record ID</label>
-          <input type="text" required id="rid" ref={ridInputRef}></input>
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='date'>Date</label>
-          <input type="text" required id="date" ref={dateInputRef}></input>
-        </div>
-        <div className={classes.actions}>
-          <button>Add Record</button>
-        </div>
-      </form>
-    </Card>
+    <>
+      <Card>
+        <form className={classes.form} onSubmit={submitHandler}>
+          <div className={classes.control}>
+            <label htmlFor='title'>Title</label>
+            <input type="text" required id="title" ref={titleInputRef}></input>
+          </div>
+          <div className={classes.control}>
+            <label htmlFor='description'>Description</label>
+            <textarea id='description' required rows='5' ref={descriptionInputRef}></textarea>
+          </div>
+          <div className={classes.control}>
+            <label htmlFor='rid'>Record ID</label>
+            <input type="text" required id="rid" ref={ridInputRef}></input>
+          </div>
+          <div className={classes.control}>
+            <label htmlFor='date'>Date</label>
+            <input type="text" required id="date" ref={dateInputRef}></input>
+          </div>
+          <div className={classes.actions}>
+            <button onClick={handleClick}>Add Record</button>
+          </div>
+        </form>
+      </Card>
+      {popUpIsOpen && (
+        <PopUp onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {popUpIsOpen && <Backdrop onCancel={closeModalHandler} />}
+    </>
+
   );
 }
 
